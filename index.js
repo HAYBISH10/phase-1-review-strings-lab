86378f0dc25874e33763cc9e5dd5e483d06ef1fe
HAYBISH10 <hassanissackmohamed172@gmail.com>
2025-03-08 01:18:28 +0300

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
-
-
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
-
-
+let currentUser = "Hassan";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
